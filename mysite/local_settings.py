549f92a2f3d632f933a546bbfe486303da648024
Jukka Välimaa <jukka.valimaa@gmail.com>
2012-09-10 23:46:56 +0300

    Changes to support using startproject

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -18,13 +18,13 @@ SECRET_KEY = '{{ secret_key }}'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '{{ project_directory }}/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT =  ''
+STATIC_ROOT =  '{{ project_directory }}/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
